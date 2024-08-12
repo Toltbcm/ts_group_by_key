7ea60c723884b2894f4a29acb9ed099618bb55f5
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2024-08-12 22:45:16 +0300

    refactor

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -4,11 +4,11 @@ type GroupsMap<T> = {
 
 export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
   const result: GroupsMap<T> = Object.fromEntries(
-    items.map((item) => [item[key] as string, []]),
+    items.map((item) => [String(item[key]), []]),
   );
 
   for (const item of items) {
-    result[item[key] as string].push(item);
+    result[String(item[key])].push(item);
   }
 
   return result;
