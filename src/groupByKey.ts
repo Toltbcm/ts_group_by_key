4efc2babfe013ae3d728e5973a2c5b04e15dbd31
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2024-08-12 21:32:55 +0300

    create groupByKey function

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,14 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T[], key: keyof T): GroupsMap<T> {
+  const result: GroupsMap<T> = Object.fromEntries(
+    items.map((item) => [item[key] as string, []]),
+  );
+
+  for (const item of items) {
+    result[item[key] as string].push(item);
+  }
+
+  return result;
 }
